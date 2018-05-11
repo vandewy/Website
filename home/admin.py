76aed4398bf4e4188b26518387204606716a2104
vandewy <vandewynkel@gmail.com>
2018-05-11 13:23:14 -0500

    bootstrap practice

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
 from .models import Personal
 
+#enables viewing and editing DB from admin page
 admin.site.register(Personal)
--- home/views.py
+++ home/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Personal
+from django.http import Http404
 
 def index(request):
-    return HttpResponse('<h1> future home page </h1>')
+    me = Personal.objects.all()
+
+    #django automatically starts at templates
+    #standard convention nameing 'context'/ is a dictionary
+    context = {
+        'me': me,
+    }
+    #httpresponse is built into render
+    return render(request, 'home/index.html', context)
