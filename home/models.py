e9245d8389cffcbb9fe241b6868205de3254f392
vandewy <vandewynkel@gmail.com>
2018-05-11 10:44:05 -0500

    personal model created

--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Personal(models.Model):
+
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    contact_number = models.CharField(max_length=25)
+    email = models.CharField(max_length=25)
+    github_account = models.CharField(max_length=100)
+    linkedin_account = models.CharField(max_length=100)
+
+
