6a4d7a0acade8711f535e0bdc2b6793e59d059e9
vandewy <vandewynkel@gmail.com>
2018-07-19 17:16:37 -0500

    email capability added

---
+++ home/templates/home/send_mail.py
@@ -0,0 +1,8 @@
+
+class Sender:
+    def __init__(self, user_name):
+        self.name = user_name
+
+    def send(self, data):
+        print(data)
+
--- home/views.py
+++ home/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from .models import Personal
 from django.http import Http404
+import cgi
+import smtplib
+import sqlite3
+import datetime
+
+
+def send_email(user_name=None, user_email=None, user_message=None):
+    print(user_name)
 
 def index(request):
     me = Personal.objects.all()
@@ -27,7 +35,7 @@ def contact(request):
     context = {
         'me': me,
     }
-    return render(request, 'home/contact.php', context)
+    return render(request, 'home/contact.html', context)
 
 def radar_sim(request):
 
@@ -48,3 +56,35 @@ def testing(request):
 def web_scraping(request):
 
     return render(request, 'home/web_scraping.html')
+
+def message_sent(request):
+
+    if request.method == 'GET':
+        name = request.GET.get('name')
+        email = request.GET.get('user_email')
+        message = request.GET.get('message')
+        date = datetime.datetime.today()
+
+
+        db = sqlite3.connect('db.sqlite3')
+        cur = db.cursor()
+        cur.execute('''INSERT INTO messages (name, email, message, date_sent)
+                          VALUES(?,?,?,?)''', (name, email, message, date,))
+        db.commit()
+        cur.close()
+        db.close()
+
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login("vandewynkel@gmail.com", "doeb pgqt fdnw pokc")
+
+        complete_message = '\n'
+        complete_message += 'Name: ' + name + '\n'
+        complete_message += 'Email: ' + email + '\n'
+        complete_message += 'Message: ' + message + '\n'
+
+        server.sendmail("vandewynkel@gmail.com", "vandewynkel@gmail.com", complete_message)
+        server.quit()
+
+    return render(request, 'home/message_sent.html')
+
